4f31620ad95d0968fe22e335ddc4dccd0ae7b784
WissalJabaoui <wissal.jabaoui@stu.ocsb.ca>
2026-04-29 13:56:25 -0400

    Add files via upload

---
+++ Beating Heart - python code  (wissal jabaoui) -main.py
@@ -0,0 +1,11 @@
+
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.show(Image.HEART)
+    sleep(400)
+    display.show(Image.HEART_SMALL)
+    sleep(400)
